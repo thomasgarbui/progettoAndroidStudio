09ebf1d2c6c6935fa53e2539218739fee0b0e5f3
thomas <53865372+thomasgarbui@users.noreply.github.com>
2024-05-15 11:01:59 +0200

    implementazione firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
 }
