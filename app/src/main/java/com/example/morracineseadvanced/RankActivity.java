f4c862b59bee2af697dd26d99eaf86842e89e4e9
willyyyyyy0000333 <163845342+SQZJR@users.noreply.github.com>
2024-05-24 18:19:10 +0200

    Test regole e garfica amici e classifica

---
+++ app/src/main/java/com/example/morracineseadvanced/RankActivity.java
@@ -0,0 +1,36 @@
+package com.example.morracineseadvanced;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RankActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_rank);
+
+        Button btn_back = findViewById(R.id.button_back);
+        Button btn_play = findViewById(R.id.button_game);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RankActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RankActivity.this, GameActivity.class);
+            }
+        });
+    }
+}
