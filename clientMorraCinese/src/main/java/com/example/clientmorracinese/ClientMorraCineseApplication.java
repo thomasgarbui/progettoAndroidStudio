34ff2e1324a2f53bc3bad090eae5d048d929d6a5
thomas <53865372+thomasgarbui@users.noreply.github.com>
2024-05-24 10:24:45 +0200

    Client funzionante

---
+++ clientMorraCinese/src/main/java/com/example/clientmorracinese/ClientMorraCineseApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientmorracinese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMorraCineseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientMorraCineseApplication.class, args);
+    }
+
+}
